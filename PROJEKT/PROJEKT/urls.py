994689b77d809ae10d8a184d3b5ac58641556c28
Németh Balázs <nemeth.balazs.19f@szlgbp.com>
2022-03-29 10:51:11 +0200

    hello world

--- PROJEKT/PROJEKT/urls.py
+++ PROJEKT/PROJEKT/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from APP.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # and define a url for it
+    path('', index),
 ]
